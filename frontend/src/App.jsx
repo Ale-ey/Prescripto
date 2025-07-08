9e0ac52d776853f1ae609e49b31b9d0382785df4
Aleey <aleeyhaider986@gmail.com>
2025-07-08 12:53:21 +0500

    add navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,29 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
+import { NavBar } from "./components/NavBar";
+import { Home } from "./pages/Home";
+import { Docters } from "./pages/Docters";
+import { Appointment } from "./pages/Appointment";
+import { Login } from "./pages/Login";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { MyAppointments } from "./pages/MyAppointments";
+import { MyProfile } from "./pages/MyProfile";
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/docters" element={<Docters />}></Route>
+        <Route path="/docters/:speciality" element={<Docters />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/my-profile" element={<MyProfile />}></Route>
+        <Route path="/my-appoitments" element={<MyAppointments />}></Route>
+        <Route path="/appointment/:docId" element={<Appointment />}></Route>
+      </Routes>
+    </div>
+  );
 };
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
+import { NavLink, useNavigate } from "react-router-dom";
+export const NavBar = () => {
+  const navigate = useNavigate();
+  const handleCreateAccount = () => {
+    navigate("/login");
+  };
+  const [showMenu, setShowMenu] = useState(false);
+  const [token, setToken] = useState(true);
+  return (
+    <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
+      <img src={assets.logo} alt="logo" className="w-44 cursor-pointer" />
+      <ul className="hidden md:flex items-start gap-5 font-medium">
+        <li className="py-1">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive
+                ? "text-primaryBlue font-semibold relative after:block after:h-0.5 after:w-3/5 after:bg-primaryBlue after:mx-auto"
+                : "text-black"
+            }
+          >
+            Home
+          </NavLink>
+        </li>
+        <li className="py-1">
+          <NavLink
+            to="/docters"
+            className={({ isActive }) =>
+              isActive
+                ? "text-primaryBlue font-semibold relative after:block after:h-0.5 after:w-3/5 after:bg-primaryBlue after:mx-auto"
+                : "text-black"
+            }
+          >
+            All Docters
+          </NavLink>
+        </li>
+        <li className="py-1">
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              isActive
+                ? "text-primaryBlue font-semibold relative after:block after:h-0.5 after:w-3/5 after:bg-primaryBlue after:mx-auto"
+                : "text-black"
+            }
+          >
+            About
+          </NavLink>
+        </li>
+        <li className="py-1">
+          <NavLink
+            to="/contact"
+            className={({ isActive }) =>
+              isActive
+                ? "text-primaryBlue font-semibold relative after:block after:h-0.5 after:w-3/5 after:bg-primaryBlue after:mx-auto"
+                : "text-black"
+            }
+          >
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+
+      <div className="flex items-center gap-4">
+        {token ? (
+          <div className="flex items-center gap-2 cursor-pointer group relative">
+            <img
+              src={assets.profile_pic}
+              alt="profilepic"
+              className="w-8 rounded-full"
+            />
+            <img src={assets.dropdown_icon} alt="dropdown" className="w-2.5" />
+            <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
+              <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
+                <p
+                  onClick={() => {
+                    navigate("/my-profile");
+                  }}
+                  className="cursor-pointer hover:text-black"
+                >
+                  My Profile
+                </p>
+                <p
+                  onClick={() => {
+                    navigate("/my-appointments");
+                  }}
+                  className="cursor-pointer hover:text-black"
+                >
+                  My Appointments
+                </p>
+                <p
+                  onClick={() => {
+                    setToken(false);
+                  }}
+                  className="cursor-pointer hover:text-black"
+                >
+                  Logout
+                </p>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <button
+            onClick={handleCreateAccount}
+            className="bg-primaryBlue text-white px-8 rounded-full font-light hidden md:block py-3 cursor-pointer"
+          >
+            {" "}
+            Create Account
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
