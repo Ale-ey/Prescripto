4f6ccef7dcc85d2e71c882d077a17ebd70340465
Aleey <aleeyhaider986@gmail.com>
2025-07-09 11:53:07 +0500

    added header section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
-
+import React from "react";
+import { Header } from "../components/Header";
+import { SpecialityMenu } from "../components/specialityMenu";
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Header />
+      <SpecialityMenu />
+    </div>
+  );
+};
